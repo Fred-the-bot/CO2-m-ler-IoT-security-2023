c70c8d42460fa11a0c46d56e2b1d600ae00ee65a
Technomancer <32704145+Fred-the-bot@users.noreply.github.com>
2023-05-07 14:22:39 +0200

    Update server(temp).py

--- server/server(temp).py
+++ server/server(temp).py
@@ -1,7 +1,16 @@
-
+import socket
 import random
+import time
 
+s = socket.socket()
+s.bind(('0.0.0.0', 8090))
+s.listen(0)
 
+print("waiting for client")
+client, addr = s.accept()
+print(client)
+
+content = client.recv(32)
 # The modulus and generator for the Diffie-Hellman key exchange
 p = 29807
 g = 3
@@ -14,11 +23,17 @@ server_public_key = pow(g, server_secret_key, p)
 
 # Wait for the client's public key
 print(server_public_key)
-client_public_key = int(input("Enter the client's public key: "))
+client_public_key = int(content)
+# client_public_key = int(input("Enter the client's public key: "))
 
 # Calculate the shared secret key
 shared_secret_key = pow(client_public_key, server_secret_key, p)
 
-
 print("Shared secret key:", shared_secret_key)
+server_public_key = str(server_public_key)
+server_public_key = server_public_key.encode('utf-8')
 
+client.sendall(server_public_key)
+
+print("Closing connection")
+client.close()
